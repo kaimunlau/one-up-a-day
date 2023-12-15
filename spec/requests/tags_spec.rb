b16475c781623ac4f6a3cf4c63b907fce9f0b42f
David Lau <david.kaimun.lau@gmail.com>
2023-12-15 21:59:02 +0100

    test dates in index

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Tags', type: :request do
   describe 'GET /index' do
-    before { get '/tags' }
     it 'returns http success' do
+      get '/tags/'
       expect(response).to have_http_status(:success)
     end
 
     it 'assigns @tags' do
       tag = Tag.create(title: 'ruby')
+      get '/tags/'
       expect(assigns(:tags)).to eq([tag])
+    end
+
+    it 'assigns @dates' do
+      tag = Tag.create(title: 'ruby')
+      post = UpdatePost.create(title: 'test', content: 'test', date: Date.today, user: create(:user))
+      post.tags << tag
+      get '/tags/'
+      expect(assigns(:dates)).to eq([Date.today])
     end
   end
 end
