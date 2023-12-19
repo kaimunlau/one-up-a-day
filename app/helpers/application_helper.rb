91638fe3563837b392d0c149ca6810fa98231713
David Lau <david.kaimun.lau@gmail.com>
2023-12-19 18:15:35 +0100

    move filters to dedicated component

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def filter_button_link_to(text, path)
+    link_to text, path,
+            class: "px-2 py-1 text-xs text-gray-600 bg-gray-200 rounded-full #{current_page?(path) ? 'bg-gray-400 font-semibold text-white' : ''}"
+  end
 end
